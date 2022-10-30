8d38342f68cfba7363393917dd7c9494ef8c934b
Nour Okbi <73375144+nourokbi@users.noreply.github.com>
2022-10-30 02:04:50 +0200

    Add project files

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const toggle = document.querySelector(".toggle")
+
+toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active")
+    document.body.classList.toggle("darkmode");
+})
